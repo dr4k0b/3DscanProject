8e0d0f093e9367812beed4e76b78171ac53ab348
wimchi <wilma.gustavson02@gmail.com>
2026-01-28 13:30:02 +0100

    movement

--- 3DscanProject/Assets/Scripts/Movement_Script.cs
+++ 3DscanProject/Assets/Scripts/Movement_Script.cs
@@ -1,16 +1,29 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class Movement_Script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    public CharacterController controller;
+
+    public float speed = 6f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Vertical");
+        Vector3 Direction = new Vector3(Horizontal, 0f, Vertical).normalized;
+
+        if (Direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf .Atan2(Direction.x, Direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            controller.Move(Direction * speed * Time.deltaTime);
+        }
+
     }
 }
