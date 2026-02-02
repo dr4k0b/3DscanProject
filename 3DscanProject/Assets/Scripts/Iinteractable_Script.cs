5733b457669a25ab6a967644fea8036c53af1143
wimchi <wilma.gustavson02@gmail.com>
2026-02-02 14:10:20 +0100

    gubbe_movement

--- 3DscanProject/Assets/Scripts/Iinteractable_Script.cs
+++ 3DscanProject/Assets/Scripts/Iinteractable_Script.cs
@@ -5,3 +5,8 @@ public interface Iinteractable_Script
     public string InteractionPrompt { get; }    
     public bool Interact(Interactor interactor);
 }
+
+public class Interactor
+{
+    // Add properties or methods as needed for your application.
+}
